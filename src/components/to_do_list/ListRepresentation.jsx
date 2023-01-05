ff9426a6f4f22ee92f53bd560911763fb6085b54
Lars Sandell <larssandell@outlook.com>
2023-01-05 11:25:28 +0100

    add style

--- src/components/to_do_list/ListRepresentation.jsx
+++ src/components/to_do_list/ListRepresentation.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 export default function ListRepresentation(props) {
     const displayList = props.listItemArray.map((item) => {
-        return <div className="ToDoItem">{item}</div>;
+        return (
+            <div className="ToDoItem">
+                <li>{item}</li>
+            </div>
+        );
     });
 
     return <div className="ToDoRepresentation">{displayList}</div>;
